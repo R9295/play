06c6e5ee7d285c1fb781babbf24bedd799f6ffba
Aarnav <aarnavbos@gmail.com>
2018-06-02 19:53:03 +0530

    added fully functioning parser ported from old play

--- matches/parser.py
+++ matches/parser.py
@@ -2,7 +2,82 @@ import requests
 from .models import Match
 from django.utils.timezone import now
 from django.conf import settings
+from .rules import GameModeRules
 
+
+player_field_list = [
+    #'ability_upgrades_arr',
+    'assists','camps_stacked','creeps_stacked','account_id','total_gold',
+    #'damage',
+    #'damage_inflictor',
+    #'damage_taken',
+    'deaths','denies','gold_per_min','actions_per_min','stuns',
+    #'dn_t',
+    ##'gold_reasons',
+    'gold_spent','hero_damage','hero_healing','kill_streaks','hero_id',
+    #'hero_i',
+    #'killed',
+    #'killed_by'
+    'kills', 'last_hits', 'max_hero_hit', 'tower_damage', 'xp_per_min', 'isRadiant',
+    'kills_per_min',
+    'neutral_kills', 'roshan_kills', 'observer_uses', 'sentry_uses', 'lane',
+    #'lane_role',
+    'is_roaming',
+]
+
+class MatchObj(object):
+    '''
+    takes in:
+    data = {
+        match_id (int)
+        duration  (int)
+        first_blood_time (int)
+        game_mode (int)
+        radiant_win (bool)
+        players  (json array with player_data dicts)
+        patch  (int)
+        region (int)
+        user (django user instance)
+    }
+    '''
+    def __init__(self, data):
+        self.data = data
+        self.parse()
+
+    def find_user(self):
+        player = None
+        # finds the user
+        for i in self.data['players']:
+            if i['account_id'] is not None and int(i['account_id']) == self.data['user'].dotaid:
+                player = i
+        # extracts necessary user data
+        self.data['user_data'] = {}
+        for i in player_field_list:
+            self.data['user_data'][i] = player.get(i)
+        # remove unnecessary list
+        self.data.pop('players', None)
+
+    def parse_dota_api_ids(self):
+        self.data['game_mode'] = GameModeRules.get(self.data['game_mode'])
+
+    def parse_user_win(self):
+        if self.data['user_data'].get('isRadiant') == True and self.data['radiant_win'] == True:
+            self.data['user_win'] = True
+        elif self.data['user_data'].get('isRadiant') == False and self.data['radiant_win'] == False:
+            self.data['user_win'] = True
+        # set to false if conditions not met
+        self.data.setdefault('user_win', 'False')
+        self.data.pop('radiant_win', None)
+        self.data['user_data'].pop('isRadiant', None)
+
+    def parse(self):
+        self.find_user()
+        self.parse_dota_api_ids()
+        self.parse_user_win()
+
+    def save(self):
+        Match(**dict(self.data)).save()
+        return self.data
 class MatchParser(object):
 
     def __init__(self, user, store_limit):
@@ -21,7 +96,7 @@ class MatchParser(object):
     def get_matches(self):
         self.matches = requests.get(settings.DOTA_API_URL+'/players/{0}/matches?limit={1}'.format(self.user.dotaid, self.store_limit)).json()
 
-    def parse_step_one(self):
+    def parse_and_save(self):
         '''
             Takes matches and returns only the ones that needed to be parsed(filters using latest_match_id)
             requeries opendota and only necessary the fields the json are passed on.
@@ -32,19 +107,18 @@ class MatchParser(object):
             if i['match_id'] == self.latest_match_id:
                 break
             self.total_parsed = self.total_parsed + 1
-            match = requests.get(DOTA_API_URL+'/matches/{0}'.format(i['match_id'])).json()
+            match = requests.get(settings.DOTA_API_URL+'/matches/{0}'.format(i['match_id'])).json()
             data = {
                 'match_id': match['match_id'],
                 'duration': match['duration'],
                 'first_blood_time': match['first_blood_time'],
                 'game_mode': match['game_mode'],
                 'radiant_win': match['radiant_win'],
-                'skill': match['skill'],
+            #    'skill': match['skill'],
                 'players': match['players'],
                 'patch': match['patch'],
                 'region': match['region'],
+                'user': self.user
             }
-            self.parsed.append(data)
-            
-    def parse_matches(self):
-        self.parse_step_one()
+            parsed_data = MatchObj(data=data)
+            parsed_data.save()
--- matches/tests.py
+++ matches/tests.py
@@ -4,6 +4,8 @@ from authentication.models import User
 from .parser import MatchParser
 from django.conf import settings
 from unittest import skip
+from django.utils.timezone import now
+
 
 # THE TESTS WITH SKIP TAKE A WHILE(query apis, parse ,etc), TEST ONLY WHEN NEEDED
 class MatchTestCase(TestCase):
@@ -11,12 +13,15 @@ class MatchTestCase(TestCase):
     def setUpTestData(cls):
         cls.user = User.objects.create(steamid='76561198232898112')
 
+    @skip
     def test_match_parser(self):
         parser = MatchParser(user=self.user,store_limit=settings.TEST_STORE_LIMIT)
         self.assertEqual(parser.latest_match_id, None)
 
-    @skip
+    #@skip
     def test_get_matches(self):
         parser = MatchParser(user=self.user,store_limit=settings.TEST_STORE_LIMIT)
         parser.get_matches()
         self.assertEqual(len(parser.matches), 5)
+        parser.parse_and_save()
+        self.assertEqual(Match.objects.filter(user=self.user).count(), 5)
