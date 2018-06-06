30156b7db29c268ece9f1387591a3c665a04626e
Aarnav <aarnavbos@gmail.com>
2018-06-06 18:14:21 +0530

    added front-end stuff.

--- play/static/js/index.js
+++ play/static/js/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
+//import 'bootstrap/dist/css/bootstrap.css';
 
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+class Matches extends React.Component {
+  render () {
+    let matches = window.props;
+    console.log(window.props)
+    return <Row>{matches.map(item => <Match key={item.match_id}
+                        match_id={item.match_id}/> )}</Row> ;
+  }
 }
 
-const element = <Welcome name="world" />;
+class Match extends React.Component {
+  render () {
+  return  (
+    <Col md="12" lg="12" sm="12">
+     <Card>
+       <CardBody>
+         <CardTitle>{ this.props.match_id}</CardTitle>
+         <CardSubtitle>Card subtitle</CardSubtitle>
+         <CardText>"Some quick example text to build on the card title and make up the bulk of the card's content"</CardText>
+         <Button>Button</Button>
+       </CardBody>
+     </Card>
+   </Col>
+ )
+  }
+}
+
 ReactDOM.render(
-  element,
+  <Matches />,
   document.getElementById('react')
 );
--- play/views.py
+++ play/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from matches.models import Match
+from matches.serializers import MatchSerializer
+import json
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        matches = Match.objects.filter(user=self.request.user)
+        context['matches'] = MatchSerializer(matches)
+        return context
