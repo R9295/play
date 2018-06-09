9b7c06a0095de081bf5fd788fe4f2b37f8d6b87f
Aarnav <aarnavbos@gmail.com>
2018-06-10 00:51:09 +0530

    init User profiles

--- play/settings.py
+++ play/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'play',
     'matches',
     'api',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
---
+++ user_profile/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+import uuid
+
+class Server(models.Model):
+    name = models.CharField(max_length=32)
+
+class Hero(models.Model):
+    name = models.CharField(max_length=32)
+
+class Role(models.Model):
+    name = models.CharField(max_length=32)
+
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key = True, default=uuid.uuid4)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    fav_servers = models.ManyToManyField(Server)
+    fav_heroes = models.ManyToManyField(Hero)
+    fav_roles = models.ManyToManyField(Role)
